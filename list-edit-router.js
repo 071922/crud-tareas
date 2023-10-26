517616fef77b8c184eeeb38e053f2387a1e45440
071922 <winanv@hotmail.es>
2023-10-25 22:16:32 -0500

    ...

--- list-edit-router.js
+++ list-edit-router.js
@@ -4,19 +4,45 @@ const router = express.Router()
 
 module.exports = router
 
+const lista = [
+    {indicador: 1, descripcion: "desc1", estado: "pendiente"},
+    {indicador: 2, descripcion: "desc2", estado: "completada"},
+    {indicador: 3, descripcion: "desc3", estado: "pendiente"},
+]
 
 router.post('/crearTarea', (req, res) => {
     
+    const tarea = {
+        indicador: req.body.indicador,
+        descripcion: req.body.descripcion,
+        estado: 'pendiente'
+    }
+    lista.push(tarea)
+
     res.send('guardado correctamente')
 })
 
-router.put('/actualizarProducto/:indicador', async (req, res) => {
+router.put('/actualizarTarea/:indicador', async (req, res) => {
+
+    const tarea = lista.find((item) => item.indicador === req.params.indicador)
+    if(!tarea) return res.status(404).send('Tarea no encontrada')
+
+    lista.map((item) => {
+        if(item.indicador === req.params.indicador){
+            item.estado = 'completada'
+        }
+    })
     
     res.send('actualizado correctamente')
 })
 
 router.delete('/borrarProducto/:indicador', async (req, res) => {
     
-    res.send('Eliminado correctamente')
+    const tarea = lista.find((item) => item.indicador === req.params.indicador)
+    if(!tarea) return res.status(404).send('Tarea no encontrada')
+
+    lista = lista.filter((item) => item.indicador !== req.params.indicador)
+    
+    res.send('borrado correctamente')
 })
 
--- list-view-router.js
+++ list-view-router.js
@@ -4,15 +4,20 @@ const router = express.Router()
 
 module.exports = router
 
+const lista = [
+    {indicador: 1, descripcion: "desc1", estado: "pendiente"},
+    {indicador: 2, descripcion: "desc2", estado: "completada"},
+    {indicador: 3, descripcion: "desc3", estado: "pendiente"},
+]
 
 router.get('/tareasCompletas', async (req, res) => {
 
-    res.send('mostrando tareas')
+    res.send(lista.filter((item) => item.estado === 'completas'))
 })
 
-router.post('/tareasCompletas', async (req, res) => {
+router.get('/tareasIncompletas', async (req, res) => {
 
-    res.send('mostrando tareas')
+    res.send(lista.filter((item) => item.estado === 'pendientes'))
 })
 
 
