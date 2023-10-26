434b8620a95bcbe2c5622bf39f374e09cb73c2e5
071922 <winanv@hotmail.es>
2023-10-25 21:35:24 -0500

    primer commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 //import routes
 const listView = require('./list-view-router')
 const listEdit = require('./list-edit-router')
